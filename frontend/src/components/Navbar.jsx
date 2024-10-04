9c21b1e76ec323df1812a688edd7c9361937a756
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-10-04 16:34:07 +0530

    create navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from "react";
-
+import "../css/navbar.css";
 export default function () {
   return (
     <div>
-     
+      <nav className="navbar navbar-expand-lg  bg-dark ">
+        <a className="navbar-brand text-white ms-3" href="#">
+          Healthy Life
+        </a>
+
+        <a className="navbar-brand text-white ms-3" href="#">
+          XXXXXXXXX
+        </a>
+
+        <a className="navbar-brand text-white ms-3" href="#">
+          XXXXXXXXX
+        </a>
+
+        <a className="navbar-brand text-white ms-3" href="#">
+          XXXXXXXXX
+        </a>
+
+        <div className="nav-buttons">
+            <button className="btn btn-success" style={{marginRight:"20px"}}>Sigin</button>
+            <button className="btn btn-warning text-white">Sigup</button>
+        </div>
+      
+        
+      </nav>
     </div>
   );
 }
