72b5891b5a82c4e80dcaaab11d5629d5655b7b65
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-10-11 15:22:34 +0530

    create user context and new update on the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "../css/navbar.css";
 import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 export default function () {
+  const { user, setUser } = useContext(UserContext);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const handleLogout = () => {
+    setUser(null);
+    setDropdownOpen(false);
+  };
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
   return (
     <div>
       <nav className="navbar navbar-expand-lg  bg-dark ">
@@ -12,7 +23,7 @@ export default function () {
           style={{ width: "150px", height: "50px", marginLeft: "20px" }}
         />
 
-        <div style={{marginLeft:"20px"}}>
+        <div style={{ marginLeft: "20px" }}>
           <a className="navbar-brand text-white ms-3" href="#">
             XXXXXXXXX
           </a>
@@ -26,16 +37,51 @@ export default function () {
           </a>
         </div>
 
-        <div className="nav-buttons">
-          <Link to="/login">
-          <button className="btn btn-success" style={{ marginRight: "20px" }}>
-            Sigin
-          </button>
-          </Link>
-          <Link to="/register">
-          <button className="btn btn-warning text-white">Sigup</button>
-          </Link>
-        </div>
+        {!user && (
+          <>
+            <div className="nav-buttons">
+              <Link to="/login">
+                <button
+                  className="btn btn-success"
+                  style={{ marginRight: "20px" }}
+                >
+                  Sigin
+                </button>
+              </Link>
+              <Link to="/register">
+                <button className="btn btn-warning text-white">Sigup</button>
+              </Link>
+            </div>
+          </>
+        )}
+
+        {user && (
+          <>
+          
+            <a href="/cart" className="text-white p-2 cart-icon">
+              <i className="fas fa-shopping-cart shopping-cart"></i>
+            </a>
+
+            <div className="text-white user-icon d-flex align-items-center me-2" style={{marginLeft:"750px"}}>
+              <i className="fas fa-user fs-5"></i>
+              <span className="username text-white fs-5">{user.username}</span>
+              <span className="dropdown-arrow text-white fs-5 ms-2" onClick={toggleDropdown}>▼</span>
+              {dropdownOpen && (
+                <div className="dropdown-content show">
+                <li>
+                  <Link
+                    to="/"
+                    onClick={handleLogout}
+                    className="dropdown-item text-dark text-center"
+                  >
+                    Logout
+                  </Link>
+                </li>
+                </div>
+              )}
+            </div>
+          </>
+        )}
       </nav>
     </div>
   );
